b560ce5b3d5ccda30b68c62ecf622702c20c9677
Devdannetworks <90023701+Devdannetworks@users.noreply.github.com>
2024-08-18 08:12:00 -0600

    Rims code

--- RimsCode.c
+++ RimsCode.c
@@ -1,115 +1,148 @@
-// Define States
-typedef enum {
-    IDLE,
-    LIGHT_CHANGE,
-    DEFAULT_CYCLE,
-    EMERGENCY_OVERRIDE
-} State;
+#include "stdint.h"
 
-State state = IDLE; // Initialize the state variable
+// Ports
+#define SYSCTL_RCGC2_R          (*((volatile unsigned long *)0x400FE108))
+#define GPIO_PORTA_DATA_R       (*((volatile unsigned long *)0x400043FC))
+#define GPIO_PORTA_DIR_R        (*((volatile unsigned long *)0x40004400))
+#define GPIO_PORTA_AFSEL_R      (*((volatile unsigned long *)0x40004420))
+#define GPIO_PORTA_DEN_R        (*((volatile unsigned long *)0x4000451C))
+#define GPIO_PORTA_AMSEL_R      (*((volatile unsigned long *)0x40004528))
+#define GPIO_PORTA_PCTL_R       (*((volatile unsigned long *)0x4000452C))
+#define GPIO_PORTC_DATA_R       (*((volatile unsigned long *)0x400063FC))
+#define GPIO_PORTC_DIR_R        (*((volatile unsigned long *)0x40006400))
+#define GPIO_PORTC_AFSEL_R      (*((volatile unsigned long *)0x40006420))
+#define GPIO_PORTC_DEN_R        (*((volatile unsigned long *)0x4000651C))
+#define GPIO_PORTC_AMSEL_R      (*((volatile unsigned long *)0x40006528))
+#define GPIO_PORTC_PCTL_R       (*((volatile unsigned long *)0x4000652C))
+#define GPIO_PORTE_DATA_R       (*((volatile unsigned long *)0x400243FC))
+#define GPIO_PORTE_DIR_R        (*((volatile unsigned long *)0x40024400))
+#define GPIO_PORTE_AFSEL_R      (*((volatile unsigned long *)0x40024420))
+#define GPIO_PORTE_DEN_R        (*((volatile unsigned long *)0x4002451C))
+#define GPIO_PORTE_AMSEL_R      (*((volatile unsigned long *)0x40024528))
+#define GPIO_PORTE_PCTL_R       (*((volatile unsigned long *)0x4002452C))
 
-// Define constants
-#define HIGH 1 
-#define THRESHOLD 100
-#define HIGH_DENSITY_THRESHOLD 5
-#define LONG_DURATION 60
-#define SHORT_DURATION 30
-#define DEFAULT_DURATION 45
+#define SYSTICK_STCTRL					(*((volatile unsigned long *)0xE000E010))
+#define SYSTICK_STRELOAD				(*((volatile unsigned long *)0xE000E014))			
+#define SYSTICK_STCURRENT				(*((volatile unsigned long *)0xE000E018))
 
-// Variables for simulating vehicle detection and sensor data
-int sensorCounter = 0;
-int vehicleCounter = 0;
+//Pin usage masks
+#define PORTA_USING 0x1C //PA2-4 output
+#define PORTC_USING 0xF0 //PC4-7 input
+#define PORTE_USING 0x3E //PE1-5 output
+
+const uint8_t fbus = 16; //MHz
 
 // Function prototypes
-int vehicleDetected();
-void adjustLightDuration();
-void runDefaultCycle();
-void manualOverride();
-void updateStateMachine();
-void setLightDuration(int duration);
-void activateEmergencyLights();
-void startTimer(int milliseconds);
-int readSensorPin();
-int getVehicleCount();
+void Systick_Delay_1sec(void);
+void Systick_Delay(uint16_t s);
+void Systick_Init(void);
+void PortA_Init(void);
+void PortC_Init(void);
+void PortE_Init(void);
+void PortA_Output(uint8_t out);
+void PortE_Output(uint8_t out);
 
-// Dummy function to replace with actual hardware interfacing
-int readSensorPin() {
-    sensorCounter++;
-    if (sensorCounter >= 10) {
-        sensorCounter = 0; 
-        return THRESHOLD + 1;
-    }
-    return THRESHOLD - 1; 
+typedef struct state_struct {
+	uint16_t delay;
+	uint8_t outA;
+	uint8_t outE;
+	const struct state_struct* next[16];
+} state_t;
+
+const state_t s[7] = {
+	{7,0x10,0x02,{&s[0],&s[5],&s[1],&s[1],&s[1],&s[5],&s[1],&s[1],&s[0],&s[5],&s[1],&s[1],&s[1],&s[5],&s[1],&s[1]}},
+	{2,0x10,0x04,{&s[2],&s[2],&s[4],&s[4],&s[2],&s[2],&s[4],&s[4],&s[2],&s[2],&s[4],&s[4],&s[2],&s[2],&s[2],&s[4]}},
+	{7,0x04,0x08,{&s[3],&s[3],&s[3],&s[3],&s[2],&s[3],&s[3],&s[3],&s[3],&s[3],&s[3],&s[3],&s[3],&s[3],&s[3],&s[3]}},
+	{2,0x08,0x08,{&s[0],&s[5],&s[4],&s[4],&s[0],&s[5],&s[4],&s[4],&s[0],&s[5],&s[4],&s[0],&s[0],&s[5],&s[4],&s[5]}},
+	{4,0x10,0x38,{&s[0],&s[0],&s[0],&s[0],&s[2],&s[2],&s[2],&s[2],&s[0],&s[0],&s[0],&s[0],&s[0],&s[2],&s[0],&s[2]}},
+	{4,0x10,0x12,{&s[6],&s[6],&s[6],&s[6],&s[6],&s[6],&s[6],&s[6],&s[6],&s[6],&s[6],&s[6],&s[6],&s[6],&s[6],&s[6]}},
+	{3,0x10,0x02,{&s[0],&s[1],&s[1],&s[1],&s[1],&s[1],&s[1],&s[1],&s[0],&s[0],&s[1],&s[1],&s[1],&s[1],&s[1],&s[1]}}
+};
+
+#define WarnerGo      s[0]
+#define WarnerWait    s[1]
+#define FirstGo       s[2]
+#define FirstWait     s[3]
+#define WarnerWalk    s[4]
+#define FirstWalk     s[5]
+#define WarnerGoShort s[6]
+
+int main(void){ 
+	state_t currentState;
+	uint8_t condition;
+	
+	Systick_Init();
+	PortA_Init();
+	PortC_Init();
+	PortE_Init();
+	
+	currentState = WarnerGo;
+	
+	while(1){
+		PortA_Output(currentState.outA);
+		PortE_Output(currentState.outE);
+		Systick_Delay(currentState.delay);
+		condition = ((GPIO_PORTC_DATA_R & 0xF0) >> 4);
+		currentState = *(currentState.next[condition]);
+  }
 }
 
-int getVehicleCount() {
-    vehicleCounter++;
-    if (vehicleCounter >= 6) {
-        vehicleCounter = 0; 
-        return HIGH_DENSITY_THRESHOLD + 1; 
-    }
-    return HIGH_DENSITY_THRESHOLD - 1; 
+void PortA_Init(void){ 
+    int delay;
+    SYSCTL_RCGC2_R |= 0x1;
+    delay = 0;
+    GPIO_PORTA_DIR_R |= PORTA_USING;
+    GPIO_PORTA_AFSEL_R &= ~PORTA_USING;
+    GPIO_PORTA_AMSEL_R &= ~PORTA_USING;
+    GPIO_PORTA_DEN_R |= PORTA_USING;
 }
 
-void setLightDuration(int duration) {
-    startTimer(duration); 
+void PortC_Init(void){ 
+    int delay;
+    SYSCTL_RCGC2_R |= 0x4;
+    delay = 0;
+    GPIO_PORTC_DIR_R &= ~PORTC_USING;
+    GPIO_PORTC_AFSEL_R &= ~PORTC_USING;
+    GPIO_PORTC_AMSEL_R &= ~PORTC_USING;
+    GPIO_PORTC_DEN_R |= PORTC_USING;
 }
 
-void activateEmergencyLights() {
+void PortE_Init(void){ 
+    int delay;
+    SYSCTL_RCGC2_R |= PORTE_USING;
+    delay = 0;
+    GPIO_PORTE_DIR_R |= PORTE_USING;
+    GPIO_PORTE_AFSEL_R &= ~PORTE_USING;
+    GPIO_PORTE_AMSEL_R &= ~PORTE_USING;
+    GPIO_PORTE_DEN_R |= PORTE_USING;
 }
 
-void startTimer(int milliseconds) {
+void Systick_Init(void) {
+		SYSTICK_STCTRL = 0x00;
+		SYSTICK_STRELOAD = 0xFFFFFF;
+		SYSTICK_STCURRENT = 0;
+		SYSTICK_STCTRL = 0x05;
 }
 
-// Main traffic light control function (called in the main loop)
-void updateStateMachine() {
-    switch (state) {
-        case IDLE:
-            if (vehicleDetected()) {
-                state = LIGHT_CHANGE;
-            }
-            break;
-        case LIGHT_CHANGE:
-            adjustLightDuration();
-            state = IDLE;
-            break;
-        case DEFAULT_CYCLE:
-            runDefaultCycle();
-            state = IDLE;
-            break;
-        case EMERGENCY_OVERRIDE:
-            manualOverride();
-            break;
-    }
+void Systick_Delay(uint16_t s) {
+	int i = 0;
+	for (i = 0; i < s; i++)
+		Systick_Delay_1sec();
 }
 
-// Function implementations
-int vehicleDetected() {
-    int sensorReading = readSensorPin();
-    return (sensorReading > THRESHOLD) ? 1 : 0;
+void Systick_Delay_1sec(void) {
+		uint32_t microseconds = 1000000;
+		SYSTICK_STRELOAD = microseconds*fbus;
+		SYSTICK_STCURRENT = 0;
+		while (!(SYSTICK_STCTRL & 0x00010000));
 }
 
-void adjustLightDuration() {
-    int vehicleCount = getVehicleCount();
-    if (vehicleCount > HIGH_DENSITY_THRESHOLD) {
-        setLightDuration(LONG_DURATION);
-    } else {
-        setLightDuration(SHORT_DURATION);
-    }
+void PortA_Output(uint8_t out) {
+	GPIO_PORTA_DATA_R |= out;
+	GPIO_PORTA_DATA_R &= (out | ~PORTA_USING);
 }
-
-void runDefaultCycle() {
-    setLightDuration(DEFAULT_DURATION);
-}
-
-void manualOverride() {
-    activateEmergencyLights();
-}
-
-// Main loop function (or main() function)
-int main() {
-    while (1) {
-        updateStateMachine();
-    }
-    return 0;
+	
+void PortE_Output(uint8_t out) {
+	GPIO_PORTE_DATA_R |= out;
+	GPIO_PORTE_DATA_R &= (out | ~PORTE_USING);
 }
